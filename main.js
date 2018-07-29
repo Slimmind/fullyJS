07326cb7b94543f9da9bfcc783143c62eb320ad2
Slim Sabernake <slimsabernake@Slims-MacBook-Pro.local>
2018-07-29 23:30:18 +0300

    added wheel event and section's scrolling

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
 const fullyContainer = document.querySelector('.fully-container');
 const fullySections = fullyContainer.querySelectorAll('.fully-section');
 const fullyDots = fullyContainer.querySelector('.fully-dots');
+let flag = true;
+let curSection = 0;
 
-[...fullySections].map((section, counter = 0) => {
+[...fullySections].forEach((section, counter = 0) => {
   section.setAttribute('data-section', ++counter);
   let fullyDot = `<button class="fully-dot" data-section-num="${counter}">Go to slide ${counter}</button>`
   fullyDots.innerHTML += fullyDot;
 });
 
 function changeSection(index) {
-  fullyContainer.querySelector('.active-section').classList.remove('active-section');
-  fullyContainer.querySelector(`[data-section="${index}"]`).classList.add('active-section');
+  if(index >= 1 && index <= fullySections.length) {
+    fullyContainer.querySelector('.active-section').classList.remove('active-section');
+    fullyContainer.querySelector(`[data-section="${index}"]`).classList.add('active-section');
+  }
 }
 
 window.onload = () => {
@@ -22,4 +26,35 @@ window.onload = () => {
       changeSection(event.target.getAttribute('data-section-num'));
     }
   });
+  document.addEventListener('wheel', (e) => {
+    if(flag) {
+      flag = false;
+      curSection = activeSection();
+      if(direction(e.deltaY) === 'forward') {
+        changeSection(activeSection() + 1);
+      } else {
+        changeSection(activeSection() - 1);
+      }
+      setTimeout(()=> {
+        flag = true;
+      }, 1400);
+    }
+  });
+}
+function direction (delta) {
+  if(delta < 0) {
+    return 'forward'
+  } else {
+    return 'back'
+  }
+}
+function activeSection() {
+  return parseInt(document.querySelector('.active-section').getAttribute('data-section'));
+}
+// Callbacks
+function one() {
+  console.log('one');
+}
+function two() {
+  console.log('two');
 }
